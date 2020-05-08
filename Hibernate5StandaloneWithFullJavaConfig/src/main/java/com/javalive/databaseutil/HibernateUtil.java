8608f9558392c3c8310f1a46c8a92694be5812ab
Administrator <Administrator@DELL>
2020-05-08 10:06:01 +0530

    Initial commit.

---
+++ Hibernate5StandaloneWithFullJavaConfig/src/main/java/com/javalive/databaseutil/HibernateUtil.java
@@ -0,0 +1,56 @@
+package com.javalive.databaseutil;
+
+import java.util.Properties;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
+import com.javalive.entity.Student;
+
+public class HibernateUtil {
+	private static StandardServiceRegistry registry;
+	private static SessionFactory sessionFactory;
+
+	public static SessionFactory getSessionFactory() {
+		if (sessionFactory == null) {
+			try {
+				Configuration configuration = new Configuration();
+				// Hibernate settings equivalent to hibernate.cfg.xml's properties
+				Properties settings = new Properties();
+				settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+				settings.put(Environment.URL, "jdbc:mysql://localhost:3306/test?useSSL=false");
+				settings.put(Environment.USER, "root");
+				settings.put(Environment.PASS, "root");
+				settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+				settings.put(Environment.SHOW_SQL, "true");
+				settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+				// settings.put(Environment.HBM2DDL_AUTO, "create-drop");
+				configuration.setProperties(settings);
+				configuration.addAnnotatedClass(Student.class);
+				registry = new StandardServiceRegistryBuilder()
+						.applySettings(configuration.getProperties()).build();
+				sessionFactory = configuration.buildSessionFactory(registry);
+
+			} catch (Exception e) {
+				e.printStackTrace();
+				if (registry != null) {
+					StandardServiceRegistryBuilder.destroy(registry);
+				}
+			}
+		}
+		return sessionFactory;
+	}
+
+	public static void shutdown() {
+		if (registry != null) {
+			StandardServiceRegistryBuilder.destroy(registry);
+		}
+	}
+
+}
---
+++ Hibernate5StandaloneWithFullJavaConfig/src/main/java/com/javalive/main/OperationsOnTheBasisOfORM.java
@@ -0,0 +1,76 @@
+package com.javalive.main;
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.javalive.databaseutil.HibernateUtil;
+
+public class OperationsOnTheBasisOfORM {
+    public static void main(String[] args) {
+    	com.javalive.entity.Student student1 = new com.javalive.entity.Student(78,"Name73", "Address73");
+    	com.javalive.entity.Student student2 = new com.javalive.entity.Student(79,"Name74", "Address74");
+        Transaction transaction = null;
+        System.out.println("Table contents before starting any operation:");
+        new OperationsOnTheBasisOfORM().getStudentList();
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            // start a transaction
+            transaction = session.beginTransaction();
+            System.out.println("\n\n");
+            // save the student objects
+            session.save(student1);
+            session.save(student2);
+            // commit transaction
+            transaction.commit();
+            System.out.println("Displaying student list after inserting new entires");
+            new OperationsOnTheBasisOfORM().getStudentList();
+            // update the student objects
+            student1.setAddress("NewName73");
+            student1.setName("NewName73");
+            student2.setAddress("NewName74");
+            student2.setName("NewName74");
+            transaction = session.beginTransaction();
+            System.out.println("\n\n");
+            session.update(student1);
+            session.update(student2);
+            // commit transaction
+            transaction.commit();
+            System.out.println("Displaying student list after updating the entires");
+            new OperationsOnTheBasisOfORM().getStudentList();
+            // delete the student objects
+            transaction = session.beginTransaction();
+            System.out.println("\n\n");
+            session.delete(student1);
+            session.delete(student2);
+            // commit transaction
+            transaction.commit();
+            System.out.println("Displaying student list after deleting the entires");
+            new OperationsOnTheBasisOfORM().getStudentList();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+        finally {
+        	System.out.println("In the finally block.");
+        	HibernateUtil.shutdown();//Closing all open resources.
+        }
+    }
+    public void getStudentList() {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            List < com.javalive.entity.Student > students = session.createQuery("from Student").list();
+			for(com.javalive.entity.Student s:students){
+				System.out.println(s.toString());
+			};
+        } catch (Exception e) {
+           e.printStackTrace();
+        }
+        /*finally {
+        	System.out.println("In the finally block.");
+        	HibernateUtil.shutdown();
+        }*/
+    }
+    
+    
+}
